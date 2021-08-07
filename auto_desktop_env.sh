6fadaa730e9b3f0c40c293f55fb85b82a238e5e3
Peter Gabaldon <pedro.gabaldonj@um.es>
2021-08-08 00:00:30 +0200

    Pre-Alpha-0.0.0.0.9 lmao

--- auto_desktop_env.sh
+++ auto_desktop_env.sh
@@ -9,24 +9,6 @@ then
   exit 1
 fi
 
-# Colors
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
-LIGHTPURPLE='\033[1;35m'
-LIGHTCYAN='\033[1;36m'
-WHITE='\033[1;37m'
-
 # Clone repo to geit config files
 git clone https://github.com/PeterGabaldon/MyDeskEnv.git "/home/$SUDO_USER/MyDeskEnv"
 
